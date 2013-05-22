9aa2d8a40ad0480e06a3610c8efb51ee491f120d
hg <devnull@localhost>
2013-05-22 17:41:21 +0200

    test configuration

--- src/test/java/com/synaway/oneplaces/test/UserServiceIntegrationTest.java
+++ src/test/java/com/synaway/oneplaces/test/UserServiceIntegrationTest.java
@@ -1,14 +1,32 @@
 package com.synaway.oneplaces.test;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.synaway.oneplaces.model.User;
+import com.synaway.oneplaces.service.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceIntegrationTest extends AbstractIntegrationTest {
+	
+	@Autowired
+	UserService userService;
 
 	@Test
-	public void getTokenShouldReturnPropperData(){
+	public void getTokenShouldReturnPropperData() throws NoSuchAlgorithmException{
+		User user = new User();
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setPassword("password");
+		user.setLogin("john");
+		user.setEmail("john@doe.pl");
 		
+		userService.saveUser(user);
+		
+		//userService.getToken(login, password);
 	}
 }
