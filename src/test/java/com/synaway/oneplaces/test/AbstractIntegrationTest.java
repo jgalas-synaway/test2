d24239a02bceecdccb67a48b19f464b2c0b99183
pawelp <devnull@localhost>
2013-07-19 17:43:12 +0200

    Simplified Spring Data JPA configuration.

--- src/test/java/com/synaway/oneplaces/test/AbstractIntegrationTest.java
+++ src/test/java/com/synaway/oneplaces/test/AbstractIntegrationTest.java
@@ -2,7 +2,7 @@ package com.synaway.oneplaces.test;
 
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(locations = {"file:src/main/resources/META-INF/spring/application-context.xml", "file:src/main/webapp/WEB-INF/1places-servlet.xml", "file:src/test/resources/META-INF/spring/application-context-test.xml" })
+@ContextConfiguration(locations = {"classpath:application-context.xml", "file:src/main/webapp/WEB-INF/1places-servlet.xml", "file:src/test/resources/META-INF/spring/application-context-test.xml" })
 abstract public class AbstractIntegrationTest {
 
 }
