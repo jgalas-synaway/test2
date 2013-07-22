4387bd36e9f0dcb8bfa87aefc450488ba3814929
pawelp <devnull@localhost>
2013-07-22 17:15:07 +0200

    Implemented Configuration best practice to allow external configuration.

--- src/test/java/com/synaway/oneplaces/test/AbstractIntegrationTest.java
+++ src/test/java/com/synaway/oneplaces/test/AbstractIntegrationTest.java
@@ -2,7 +2,7 @@ package com.synaway.oneplaces.test;
 
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(locations = {"classpath:application-context.xml", "file:src/main/webapp/WEB-INF/1places-servlet.xml", "file:src/test/resources/META-INF/spring/application-context-test.xml" })
+@ContextConfiguration(locations = {"classpath:application-context.xml", "file:src/main/webapp/WEB-INF/1places-servlet.xml", "file:src/test/resources/application-context-test.xml" })
 abstract public class AbstractIntegrationTest {
 
 }
