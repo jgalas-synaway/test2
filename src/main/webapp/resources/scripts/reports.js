b712b32322f19d83f38a4c88b1e0bd477b47725b
pawelp <devnull@localhost>
2013-08-02 15:47:02 +0200

    Added grid to the activity report map.

--- src/main/webapp/resources/scripts/reports.js
+++ src/main/webapp/resources/scripts/reports.js
@@ -77,7 +77,11 @@ L.TileLayer.Ajax = L.TileLayer.extend({
     },
     _addTileData: function(tile) {
     	tile.marker = new L.Marker(tile.latlon, {
-    		icon:	new L.NumberedDivIcon({number: JSON.parse(tile.datum).greenRedClickCount})
+    		icon: new L.divIcon({
+    			className: 'leaflet-click-count',
+    			iconAnchor: new L.Point(0, -128),
+    			iconSize: new L.Point(256, 256),
+    			html: JSON.parse(tile.datum).greenRedClickCount})
     	})
     	tile.marker.addTo(this._map);
     },
@@ -129,38 +133,4 @@ L.TileLayer.Ajax = L.TileLayer.extend({
         if (this._tilesToLoad < 0) this._tilesToLoad = 0;
         L.TileLayer.prototype._update.apply(this, arguments);
     }
-});
-
-L.NumberedDivIcon = L.Icon.extend({
-	options: {
-    // EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
-    iconUrl: 'http://www.charliecroom.com/marker_hole.png',
-    number: '',
-    shadowUrl: null,
-    iconSize: new L.Point(25, 41),
-		iconAnchor: new L.Point(13, 41),
-		popupAnchor: new L.Point(0, -33),
-		/*
-		iconAnchor: (Point)
-		popupAnchor: (Point)
-		*/
-		className: 'leaflet-div-icon'
-	},
-
-	createIcon: function () {
-		var div = document.createElement('div');
-		//var img = this._createImg(this.options['iconUrl']);
-		var numdiv = document.createElement('div');
-		numdiv.setAttribute ( "class", "number" );
-		numdiv.innerHTML = this.options['number'] || '';
-		//div.appendChild ( img );
-		div.appendChild ( numdiv );
-		this._setIconStyles(div, 'icon');
-		return div;
-	},
-
-	//you could change this to add a shadow like in the normal marker if you really wanted
-	createShadow: function () {
-		return null;
-	}
 });
